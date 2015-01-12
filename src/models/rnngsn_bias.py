2f178134b1ea79cb886ec199fa9649a9d1d6d7ba
mbeissinger <mbeissinger@gmail.com>
2015-01-12 16:50:12 -0500

    going to run rnngsn bias on aws

--- src/models/rnngsn_bias.py
+++ src/models/rnngsn_bias.py
@@ -27,15 +27,17 @@ def experiment(state, outdir_base='./'):
     data.mkdir_p(outdir)
     logger = Logger(outdir)
     logger.log("----------MODEL 2, {0!s}-----------\n".format(state.dataset))
-    gsn_train_convergence = outdir+"gsn_train_convergence.csv"
-    gsn_valid_convergence = outdir+"gsn_valid_convergence.csv"
-    gsn_test_convergence  = outdir+"gsn_test_convergence.csv"
+    if state.initialize_gsn:
+        gsn_train_convergence = outdir+"gsn_train_convergence.csv"
+        gsn_valid_convergence = outdir+"gsn_valid_convergence.csv"
+        gsn_test_convergence  = outdir+"gsn_test_convergence.csv"
     train_convergence = outdir+"train_convergence.csv"
     valid_convergence = outdir+"valid_convergence.csv"
     test_convergence  = outdir+"test_convergence.csv"
-    init_empty_file(gsn_train_convergence)
-    init_empty_file(gsn_valid_convergence)
-    init_empty_file(gsn_test_convergence)
+    if state.initialize_gsn:
+        init_empty_file(gsn_train_convergence)
+        init_empty_file(gsn_valid_convergence)
+        init_empty_file(gsn_test_convergence)
     init_empty_file(train_convergence)
     init_empty_file(valid_convergence)
     init_empty_file(test_convergence)
@@ -476,13 +478,13 @@ def experiment(state, outdir_base='./'):
     #############################
     def save_params_to_file(name, n, gsn_params):
         pass
-        print 'saving parameters...'
-        save_path = outdir+name+'_params_epoch_'+str(n)+'.pkl'
-        f = open(save_path, 'wb')
-        try:
-            cPickle.dump(gsn_params, f, protocol=cPickle.HIGHEST_PROTOCOL)
-        finally:
-            f.close()
+#         print 'saving parameters...'
+#         save_path = outdir+name+'_params_epoch_'+str(n)+'.pkl'
+#         f = open(save_path, 'wb')
+#         try:
+#             cPickle.dump(gsn_params, f, protocol=cPickle.HIGHEST_PROTOCOL)
+#         finally:
+#             f.close()
             
     def save_params(params):
         values = [param.get_value(borrow=True) for param in params]
